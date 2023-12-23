35f94de66f94c1bf18dba001636ee29a8f53c875
kabird1 <148470042+kabird1@users.noreply.github.com>
2023-12-23 00:23:34 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -77,10 +77,9 @@ def no_button_callback():
 
 #user uploads file here
 #when user uploads new file, counter is reset, and the first image is loaded
-def rerun():
-    st.rerun()
+
 if st.session_state.user_file==None:
-    st.session_state.user_file=st.file_uploader(label="Upload CSV", type={"csv","txt"}, help="CSV File containg the following columns X-coordinate, Y-Coordinate, Feature, Yes/No.", on_change=rerun)
+    st.session_state.user_file=st.file_uploader(label="Upload CSV", type={"csv","txt"}, help="CSV File containg the following columns X-coordinate, Y-Coordinate, Feature, Yes/No.")
 
 if st.session_state.user_file!=None:
     if st.session_state.counter==0:
